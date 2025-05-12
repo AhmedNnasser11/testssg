d31bc97ed550c4c6a7ac2324ca01ecef7dae21c8
ahmednnasser11 <Ahmednnasser11@gmail.com>
2025-05-12 12:58:18 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["picsum.photos"], // Use picsum.photos instead of via.placeholder.com
+    unoptimized: process.env.NODE_ENV === 'development', // Optional: Disable optimization in development
+  },
 };
 
 export default nextConfig;
